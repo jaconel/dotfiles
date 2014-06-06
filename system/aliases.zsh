81dc0c44ab2c8b665d041563d419a5882ff02ff8
Jaco Nel <jacon@rightshift.biz>
2014-06-06 10:42:05 +0200

    Still cleaning up aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,13 +9,11 @@ alias ack='ack-grep'
 alias untar="tar -zxvf"
 alias p="python"
 alias l="ls --color=auto -lAhtF"
-alias open="xdg-open"
 alias e='exit'
 alias s='sudo'
 alias c='clear'
 alias shutdown='sudo shutdown –h now'
 alias restart='sudo shutdown –r now'
-alias lock='gnome-screensaver-command --lock'
 alias mounted='mount | column –t'
 alias virt='virtualenv --no-site-packages --prompt=\(venv:$(pwd|sed "s/.*\///")\) venv/; source venv/bin/activate'
 
